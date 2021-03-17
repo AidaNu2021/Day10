5a722b879fb25ca0f4c60a61db88e7a32c63dd4c
AidaNu2021 <aida.nusinovic@gmail.com>
2021-03-17 22:54:08 +0100

    dodat ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,15 +1,17 @@
-
 var lion = {
     "name": "Simba",
     "legs": 4,
     "tails": 1,
 };
-
-function myFunction(lion, roar) {
-    lion ["roar"] = "roar-roar";
-    return lion;
-    console.log(myFunction("roar", "roar-roar"));
+function myFunction(roar) {
+    console.log(roar);
 }
+function addProperty(propName, propValue) {
+    lion["roar"] = "roar-roar";
+    return lion;
+}
+    console.log(addProperty("roar", "roar-roar"));
+
 myFunction(lion);
 module.exports = myFunction;
 
