b731cc61af227a439228276e4da59f9213ae90c3
AidaNu2021 <aida.nusinovic@gmail.com>
2021-03-17 21:27:47 +0100

    dodat ex01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,14 @@
 
+function myFunction(myObj) {
+    return myObj;
+}
+var myPet = {
+    species: "Maltezer",
+    name: "Bobi",
+    legs: 4,
+    friends: [ 'Aida', 'Amra' ]
+};
+
+console.log(myFunction(myPet));
+
+module.exports = { myPet, myFunction };
